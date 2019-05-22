a7dafa2470eaa26a770baafe18a4dca47959e6c5
Twisttooth <safira.falck@gmail.com>
2019-05-22 19:17:04 +0300

    chapter10

---
+++ merkkijono.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main()
+{
+    char etunimi[10], sukunimi[20], tiedosto[10];
+
+    printf("Ohjelma tallentaa etunimesi ja sukunimesi tiedostoon.\n");
+    
+    printf("Syötä etunimesi:");
+    scanf("%s", etunimi);
+
+    printf("sukunimesi:");
+    scanf("%s", sukunimi);
+
+    printf("Tiedosto johon haluat tallentaa nimesi:");
+    scanf("%s", tiedosto);
+
+    FILE *nimi;
+    FILE *tarkistus;
+
+    nimi = fopen(tiedosto, "w");
+    tarkistus = fopen(tiedosto, "r");
+
+    fprintf(nimi, "%s %s", etunimi, sukunimi);
+    
+    if (tarkistus == NULL)
+    {
+        printf("tiedosto virhe");
+        return 0;
+    }
+    else
+    {
+        printf("Tallennus onnistui!");
+    }
+    
+
+    return 0;
+}
