5ced541fcfa52574ef4c4c73a719a520b4d7d714
Twisttooth <safira.falck@gmail.com>
2019-05-23 19:18:15 +0300

    chapter10progress

---
+++ vokaalit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    char sana[15], vokaali;
+    int maara;
+
+    printf("Ohjelma laskee vokaalien määriä.\nAnna sana:");
+    scanf("%s", &sana);
+
+    for (int i = 0; i < 16; i++)
+    {
+        //laskee vokaaleja? (maara+1)
+    }
+    
+    
+    return 0;
+}
+
+//Ohjelma laskee vokaalien määriä. 
+//Anna sana:testi 
+//Sanassa vokaaleja: 2
+
+// ? (char[] = "sana"; sana[1] == 'a';) ?
